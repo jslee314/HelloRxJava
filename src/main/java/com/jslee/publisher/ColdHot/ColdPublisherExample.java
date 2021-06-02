d07d78f488724e796803cc41b2b40902f9c4488a
jslee314 <jslee_314@naver.com>
2021-06-02 11:55:15 +0900

    hot publisher, cold publisher

--- src/main/java/com/jslee/publisher/ColdHot/ColdPublisherExample.java
+++ src/main/java/com/jslee/publisher/ColdHot/ColdPublisherExample.java
@@ -4,6 +4,7 @@ import io.reactivex.Flowable;
 
 public class ColdPublisherExample {
     public static void main(String[] args){
+
         Flowable<Integer> flowable = Flowable.just(1, 3, 5, 7);
 
         flowable.subscribe(data -> System.out.println("구독자1: " + data));
